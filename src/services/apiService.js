6b2657cba7e1bd3c01c9ce72b517f7e35e158793
Varshith <ellendulavarshith1@gmail.com>
2026-03-11 19:06:20 +0530

    update api url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://collab-backend-production-a2b8.up.railway.app";
 
 // ─── STUDENT ───────────────────────────────────────────────
 export const registerStudent = async (student) => {
